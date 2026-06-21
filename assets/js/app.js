9c1b3857d96480e3c7ac9f132dd05a8e309b69ff
Peter Belarmino <peterbelarmino@users.noreply.github.com>
2026-06-22 00:53:23 +0300

    Remove public gateway and redirect directly to secure login

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  document.querySelectorAll('a[href*="view=login"]').forEach(link => {
-    link.addEventListener("click", () => {
-      link.setAttribute("aria-busy", "true");
-    });
-  });
-})();
